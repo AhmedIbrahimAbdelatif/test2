7427a187144891c6e2fb7e212c68a46f09c8927d
AhmedIbrahimAbdelatif <ahmed.ibrahim11583@gmail.com>
2022-03-12 19:05:18 +0200

    initial commit

---
+++ js/mine.js
@@ -0,0 +1,17 @@
+
+//effects
+// $('#demo').hide(3000)
+// $('.btn').click(function(e){
+//   alert('jjjj')
+//   console.log(e)
+// })
+
+
+// $('.btn').click(function(){
+//   $('#demo').hide(3000,function(){
+//     $('#demo2').hide(3000,function(){
+//       $('#demo3').hide(3000)
+//     })
+//   })
+// })
+$('')
